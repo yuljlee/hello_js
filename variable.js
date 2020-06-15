c6edbdbdc2791c0f88ca2d5331347045bdadc728
Yul Jae Lee <yuljlee9@gmail.com>
2020-06-14 22:01:38 -0700

    chore: Variable test

---
+++ variable.js
@@ -0,0 +1,12 @@
+// 1. use strict
+// add in ES5
+// use this for Vanila JavaScript
+'use strict';
+
+// 2. Variable, rw(read/write)
+// let (added in ES 6)
+
+console.log("Hello World!");
+
+let a = 10;
+
