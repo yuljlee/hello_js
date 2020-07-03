f0a2fe5ccf3c354dc224224c3cd7a556fa0fda2c
Yul Jae Lee <yuljlee9@gmail.com>
2020-07-02 18:27:33 -0700

    feat: Add operator.js

---
+++ operator.js
@@ -0,0 +1,6 @@
+// 1. String concatenation
+console.log('my' + ' cat');
+console.log('1' + 2);
+const a = '1' + 2;
+console.log(`value: ${a} type: ${typeof a}`);
+console.log(`string literals: 1 + 2 = ${1 + 2}`);
--- variable.js
+++ variable.js
@@ -33,13 +33,19 @@ var age;
 }
 console.log(age);
 
-// 3. Constant
+// 3. Constant, r(read only)
+// use const whenever possible.
+// only use let if variable needs to change.
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// Note!
+// Immutable data types: primitive types, frozon objects (i.e. object.freeze())
+// Mutable data types: all objects by default are mutable in JS
 // favor immutable data type always for a few reasons:
 //  - security
 //  - thread safety
 //  - reduce human mistakes
-const daysInWeek = 7;
-const maxNumber = 5;
 
 // 4. Variable types
 // primitive, single item: number, string, boolean, null, undefined, symbol
